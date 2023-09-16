15c21605e5fab3b7967ca1000e78ae11137d1d16
Yaser Aly <yaser-aly@live.com>
2023-09-15 17:16:25 -0700

    refactored new meal and new workout into a single new item method

--- tracalorie/js/app.js
+++ tracalorie/js/app.js
@@ -120,36 +120,17 @@ class App {
 
     document
       .getElementById('meal-form')
-      .addEventListener('submit', this._newMeal.bind(this));
+      .addEventListener('submit', this._newItem.bind(this, 'meal'));
 
     document
       .getElementById('workout-form')
-      .addEventListener('submit', this._newWorkout.bind(this));
-  }
-  _newMeal(e) {
-    e.preventDefault();
-    const name = document.getElementById('meal-name');
-    const calories = document.getElementById('meal-calories');
-
-    // validate inputs
-    if (name.value === '' || calories === '') {
-      alert('Please fill in all fields');
-      return;
-    }
-    const meal = new Meal(name.value, +calories.value); // the + here before calories.value converts the value from string to number.
-    this._tracker.addMeal(meal);
-
-    name.value = '';
-    calories.value = '';
-
-    const collapseMeal = document.getElementById('collapse-meal');
-    const bsCollapse = new bootstrap.Collapse(collapseMeal, { toggle: true });
+      .addEventListener('submit', this._newItem.bind(this, 'workout'));
   }
 
-  _newWorkout(e) {
+  _newItem(type, e) {
     e.preventDefault();
-    const name = document.getElementById('workout-name');
-    const calories = document.getElementById('workout-calories');
+    const name = document.getElementById(`${type}-name`);
+    const calories = document.getElementById(`${type}-calories`);
 
     // validate inputs
     if (name.value === '' || calories === '') {
@@ -157,16 +138,19 @@ class App {
       return;
     }
 
-    const workout = new Workout(name.value, +calories.value); // the + here before calories.value converts the value from string to number.
-    this._tracker.addWorkout(workout);
+    if (type === 'meal') {
+      const meal = new Meal(name.value, +calories.value); // the + here before calories.value converts the value from string to number.
+      this._tracker.addMeal(meal);
+    } else {
+      const workout = new Workout(name.value, +calories.value); // the + here before calories.value converts the value from string to number.
+      this._tracker.addWorkout(workout);
+    }
 
     name.value = '';
     calories.value = '';
 
-    const collapseWorkout = document.getElementById('collapse-workout');
-    const bsCollapse = new bootstrap.Collapse(collapseWorkout, {
-      toggle: true,
-    });
+    const collapseItem = document.getElementById(`collapse-${type}`);
+    const bsCollapse = new bootstrap.Collapse(collapseItem, { toggle: true });
   }
 }
 
